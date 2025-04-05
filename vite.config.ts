cb8fd2b317181dd113150741bed0250d83fd6ecd
eltim12 <eltim2000@gmail.com>
2025-04-05 17:45:07 +0700

    feat: initialize & setup project

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: 'src/entry.ts',
+      name: 'Greeter',
+      fileName: 'compiled-greeter-component',
+      formats: ['iife'],
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+})
